df1ea3738266f0ab75ab324961d3707986c0a1f7
juanpablo <1719570+floydnunez@users.noreply.github.com>
2024-12-16 13:22:56 -0300

    save

--- src/main/scala/day_16.scala
+++ src/main/scala/day_16.scala
@@ -10,9 +10,10 @@ import scala.util.boundary.break
 
 case class Pos(var x: Int, var y: Int)
 
-case class Node(x: Int, y: Int, costs: mutable.Map[Char, Int], var char: Char)
-
+case class Node(x: Int, y: Int, costs: mutable.Map[Char, Int], var char: Char, var isBest: Boolean)
+var recursiveCalls = 0
 val dirs = Map('^' -> (0,-1), '>' -> (1,0), 'v' -> (0,1), '<' -> (-1,0))
+val opposite = Map('^' -> 'v', '>' -> '<', 'v' -> '^', '<' -> '>')
 val cardinal = List('^', '>', 'v', '<')
 
 var height = 0
@@ -24,8 +25,8 @@ def initCostMap(): mutable.Map[Char, Int] =
   result
 
 @main def main(): Unit =
-  val fileName = "inputs/day16/input.txt"
-  if (fileName == "inputs/day16/input.txt") {
+  val fileName = "inputs/day16/challenge_4.txt"
+  if (fileName == "inputs/day16/input.txt" || fileName.contains("challenge")) {
     debug = false
   }
   val fileContents = Source.fromFile(fileName).getLines().toArray
@@ -37,7 +38,7 @@ def initCostMap(): mutable.Map[Char, Int] =
   val fin = Pos(0,0)
   fileContents.zipWithIndex.foreach{ case(line, yindex) =>
     line.zipWithIndex.foreach{ case(char, xindex) =>
-      map((xindex, yindex)) = Node(x=xindex, y=yindex, costs = initCostMap(), char = char)
+      map((xindex, yindex)) = Node(x=xindex, y=yindex, costs = initCostMap(), char = char, isBest = false)
       if (char == 'S') {
         ini.x = xindex
         ini.y = yindex
@@ -51,18 +52,74 @@ def initCostMap(): mutable.Map[Char, Int] =
   printMap(map)
   println(s"${width} x ${height}. From ${ini} to ${fin}")
   flood(map, '>', (ini.x, ini.y), 0)
-  println(map((fin.x, fin.y)))
-  println(s"\nDay 16 Part 1: ${map((fin.x, fin.y)).costs.values.min}") //88416
+  val endNode = map((fin.x, fin.y))
+  println(endNode)
+  val minCost = endNode.costs.values.min
+  println(s"\n\nDay 16 Part 1: ${minCost}") //88416
+  println(s"recursive calls: ${recursiveCalls}")
+  //Part 2. Let's backtrack
+  println("\n\n\n\n\n\n\n\n------------------------------------------------\n\n\n\n\n\n\n\n")
+  debug = true
+  backtrackFrom(map, (endNode.x, endNode.y), Int.MaxValue)
+  printMapPath(map, minCost)
+  val bestNodesCount = map.values.count(_.isBest)
+  println(s"\nDay 16 Part 1: ${minCost}") //88416
+  println(s"\nDay 16 Part 2: ${bestNodesCount}") //442
+
+def backtrackFrom(map: mutable.Map[(Int, Int), Node], pos: (Int, Int), prevCost: Int): Unit = {
+  recursiveCalls += 1
+  if (!map.contains(pos)) {
+    return
+  }
+  val currNode = map(pos)
+  if (currNode.char == '#') {
+    return
+  }
+  printDebug(s"Backtracking on ${currNode} prevCost: ${prevCost} on ${currNode.costs}")
+  currNode.isBest = true
+  val minCost = currNode.costs.values.min
+  if (minCost == 0) { //the end! ...or start
+    return
+  }
+  var count = 0
+  currNode.costs.foreachEntry { case (dir, cost) => {
+    printDebug(s"-for each entry of node ${pos}: ${dir} ${cost}")
+      if (cost == minCost) {
+        count += 1
+        val nextDir = dirs(opposite(dir))
+        val nextPos = (pos._1 + nextDir._1, pos._2 + nextDir._2)
+        printDebug(s"   min dir: ${dir}, from: ${nextPos} to ${pos} ")
+        backtrackFrom(map, nextPos, minCost)
+      } else if (cost != Int.MaxValue) {
+        val nextDir = dirs(opposite(dir))
+        val nextPos = (pos._1 + nextDir._1, pos._2 + nextDir._2)
+        val nextNode = map(nextPos)
+        printDebug(s"   not min: ${dir} ${nextPos} ${nextNode.char} ${nextNode.costs}")
+        if (nextNode.char != '#') { //not a wall
+          val minNextCost = nextNode.costs.values.min
+          if (minNextCost + 2 == prevCost && !nextNode.isBest) {
+            //hack? just jump over if prev cost is 2 more than min next cost, ignoring the discontinuity.
+            //It happens on node (5,7) on example_1
+            printDebug("trust the bifurcation")
+            backtrackFrom(map, nextPos, minCost)
+          }
+        }
+      }
+      if (count > 1) {
+        printDebug(s"bifurcation at ${pos}")
+      }
+    }
+  }
+}
 
 def printDebug(str: String): Unit =
-  if (debug) {
+  if (debug)
     println(str)
-  }
 
 def flood(map: mutable.Map[(Int, Int), Node], dir: Char, pos: (Int, Int), cost: Int): Unit =
   val currNode = map(pos)
   calcCosts(currNode, dir, cost)
-  println(s"costs calculated! ${currNode}")
+  printDebug(s"costs calculated! ${currNode}")
   val nextPos = dirs.view.mapValues { case (dx, dy) => (pos._1 + dx, pos._2 + dy) }.toMap
   nextPos.foreachEntry{ case(nextDir, npos) =>
 //    printDebug(s"? ${nextDir} => ${npos}")
@@ -105,9 +162,33 @@ def calcCosts(node: Node, dir: Char, cost: Int):Unit =
 
 def printMap(map: mutable.Map[(Int, Int), Node]): Unit =
   var str = ""
+  for (y <- 0 until height) {
+    for (x <- 0 until width) {
+      str += map((x, y)).char
+    }
+    str += "\n"
+  }
+  println(str)
+
+def printMapPath(map: mutable.Map[(Int, Int), Node], minCost: Int): Unit =
+  var minCostSize = minCost.toString.length
+  var str = ""
   for( y <- 0 until height) {
     for( x <- 0 until width) {
-      str += map((x,y)).char
+      val node = map((x,y))
+      val minCost = node.costs.values.min
+      if (minCost == Int.MaxValue) {
+        str += node.char.toString * (minCostSize + 1)
+      } else {
+        val formatString = s"%${minCostSize }d"
+        val formattedNumber = String.format(formatString, minCost.asInstanceOf[Object])
+        var isBest = " "
+        if (node.isBest) {
+          isBest = "_"
+        }
+        str += isBest
+        str += s"${formattedNumber}"
+      }
     }
     str += "\n"
   }
